7fc7bd0ed7d869021b17f10db20b39db9072a466
Nishit Ranjan <nishranjan@deloitte.com>
2021-02-10 11:32:57 +0530

    configuring handlebars and view engine

--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const app = express();
-const connectDB = require("./config/db");
-
+const connectDB = require("./src/config/db");
+const path = require("path");
 
 //Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./src/config/config.env" });
+
+app.set('view engine', 'hbs')
+
+const pathDir = path.join(__dirname, "./public");
+app.use(express.static(pathDir));
+
+app.get("", (req,res)=>{
+    res.render('index', {
+        title: "Home Page",
+        name:"Nishit Ranjan-Home"
+    })
+})
+
+app.get("/about", (req,res)=>{
+    res.render('about', {
+        title: "About Page",
+        name:"Nishit Ranjan-About"
+    })
+})
+
+console.log(pathDir);
 
 app.use(express.json());
 
@@ -15,15 +36,12 @@ connectDB();
 
 
 
-const book = require("./routes/book");
-const author = require("./routes/author");
+const book = require("./src/routes/book");
+const author = require("./src/routes/author");
 
 app.use("/app/v1/book", book);
 app.use("/app/v1/author", author);
 
-app.use("/",(req,res)=>{
-    res.json({message:"Server is Up"})
-});
 
 app.listen(PORT,()=>{
     console.log(`Server listening on ${PORT}`.bold.cyan )
